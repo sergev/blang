d0b23d0f584196259a4312d4df972b5c1f10e719
Serge Vakulenko <serge.vakulenko@gmail.com>
2025-10-14 15:09:54 -0700

    Make progress with syscall() for aarch64 on MacOS. Not tested yet.

--- runtime/libb.c
+++ runtime/libb.c
@@ -24,24 +24,32 @@
 
 static inline SYSCALL_TYPE syscall(SYSCALL_TYPE n, SYSCALL_TYPE a1, SYSCALL_TYPE a2, SYSCALL_TYPE a3)
 {
-#ifdef __APPLE__
-    n |= 0x2000000;
-#endif
 #ifdef __aarch64__
     register long x0 asm("x0") = a1;
     register long x1 asm("x1") = a2;
     register long x2 asm("x2") = a3;
-    register long x8 asm("x8") = n; // syscall number goes in x8
-
-    // svc #0 invokes the kernel. x0 is used for return value.
-    asm volatile("svc #0"
+#ifdef linux
+    register long x8 asm("x8") = n;          // syscall number goes in x8
+    asm volatile("svc #0"                    // svc #0 invokes the kernel
                  : "+r"(x0)                  // x0 is input/output (return)
                  : "r"(x1), "r"(x2), "r"(x8) // inputs
                  : "memory");
+#endif
+#ifdef __APPLE__
+    register long x16 asm("x16") = n;         // syscall number goes in x16
+    asm volatile("svc #0x80"                  // svc #0x80 performs the syscall
+                 : "+r"(x0)                   // output: x0 returned
+                 : "r"(x1), "r"(x2), "r"(x16)
+                 : "memory");
+#endif
     return x0;
 #endif
+
 #ifdef __x86_64__
     long ret;
+#ifdef __APPLE__
+    n |= 0x2000000;
+#endif
     asm volatile("syscall"
                  : "=a"(ret)                         // output: rax <- return
                  : "a"(n), "D"(a1), "S"(a2), "d"(a3) // inputs: rax, rdi, rsi, rdx
